76491cfd7ce03d8be6a49b3e71c88cc411e40434
Joan <janisaez@mac.home>
2026-03-19 09:24:08 +0000

    exp46: GBM n=200 depth=2 lr=0.02 subsample=0.8 max_features=sqrt

--- autoresearch/projects/ct-predictor/train.py
+++ autoresearch/projects/ct-predictor/train.py
@@ -147,7 +147,7 @@ def build_features(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
 MODEL = GradientBoostingClassifier(
     n_estimators=200,
     max_depth=2,
-    learning_rate=0.05,
+    learning_rate=0.02,
     subsample=0.8,
     min_samples_leaf=5,
     max_features="sqrt",
